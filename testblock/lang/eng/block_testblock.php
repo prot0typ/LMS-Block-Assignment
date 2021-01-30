8a259671bdf2208e799a6c3f59d165a8932a502d
prot0typ <78256964+prot0typ@users.noreply.github.com>
2021-01-30 10:18:10 +0000

    Adds the core files for the assignment.

---
+++ testblock/lang/eng/block_testblock.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * Strings for component 'block_testblock', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_testblock
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['testblock:addinstance'] = 'Add a new test block';
+$string['testblock:myaddinstance'] = 'Add a new test block to Dashboard';
+$string['testblock'] = '(new test block)';
+$string['pluginname'] = 'test block';
